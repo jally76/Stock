4be4e8d9e02938147f234bfb9a53873226774eb9
landeeyo <landeeyo@10g.pl>
2014-11-11 00:14:28 +0100

    Improved domain model.

--- StockTools/StockTools/StockTools.Entities/InvestmentPortfolio.cs
+++ StockTools/StockTools/StockTools.Entities/InvestmentPortfolio.cs
@@ -13,27 +13,36 @@ namespace StockTools.Entities
         /// <summary>
         /// Sum of current prices of all shares in the portfolio
         /// </summary>
-        public double GetValue { get { throw new NotImplementedException(); } }
+        public double Value { get { throw new NotImplementedException(); } }
 
         /// <summary>
         /// Gross profit of the portfolio
         /// </summary>
-        public double GetGrossProfit { get { throw new NotImplementedException(); } }
+        public double GrossProfit { get { throw new NotImplementedException(); } }
 
         /// <summary>
         /// Net profit of the portfolio
         /// </summary>
-        public double GetNetProfit { get { throw new NotImplementedException(); } }
+        public double NetProfit { get { throw new NotImplementedException(); } }
 
         /// <summary>
         /// Sets charge function which is necessary for profit calculation
         /// </summary>
-        public Func<double,double> SetChargeFunc { set { throw new NotImplementedException(); } }
+        public Func<double, double> ChargeFunc { set { throw new NotImplementedException(); } }
+
+        /// <summary>
+        /// Sets value of capital gain tax (during the time because we assume that it can change)
+        /// </summary>
+        public List<Taxation> Taxation { set { throw new NotImplementedException(); } }
 
         public class Item
         {
             public string CompanyName { get; set; }
             public int NumberOfShares { get; set; }
+            public double BuyPrice { get; set; }
+            public double? SellPrice { get; set; }
+            public DateTime BuyDate { get; set; }
+            public DateTime? SellDate { get; set; }
 
             /// <summary>
             /// Current price of all shares of particular company
---
+++ StockTools/StockTools/StockTools.Entities/Taxation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTools.Entities
+{
+    /// <summary>
+    /// Taxation in a period of a time. Value means percentage of taxation.
+    /// </summary>
+    public class Taxation
+    {
+        public double Value { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
