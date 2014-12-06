8f9ad58e75f879e841fecf3b7ba42735b26b24d0
landeeyo <landeeyo@10g.pl>
2014-12-06 13:38:27 +0100

    Added new project (webUI) and new interfaces for downloading data.

---
+++ StockTools/StockTools/StockTools.Domain/Abstract/IIntradayDataDownloader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTools.BusinessLogic.Abstract
+{
+    public interface IIntradayDataDownloader
+    {
+        string Address { get; set; }
+        IIntradayDataParser Parser { get; set; }
+
+        void Download(string path);
+    }
+}
---
+++ StockTools/StockTools/StockTools.Domain/Abstract/IIntradayDataParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockTools.BusinessLogic.Abstract
+{
+    public interface IIntradayDataParser
+    {
+        Dictionary<string,string> GetFileAddresses(string address);
+    }
+}
