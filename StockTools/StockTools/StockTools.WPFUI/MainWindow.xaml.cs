3f877c4d7d79394bb36d95164f2c38c2f86a6e51
landeeyo <landeeyo@10g.pl>
2014-12-08 21:50:19 +0100

    Added WPF UI project due to Web UI constraints (downloading multiple files on disk to avoid compressing them into single file in memory because of timeouts).

---
+++ StockTools/StockTools/StockTools.WPFUI/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using StockTools.BusinessLogic.Abstract;
+using StockTools.BusinessLogic.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockTools.WPFUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IIntradayDataDownloader _intradayDataDownloader = new BOSSAIntradayDataDownloader();
+        IIntradayDataParser _intradayDataParser = new BOSSAIntradayDataParser();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            //var outputStream = new MemoryStream();
+            _intradayDataDownloader.Address = @"http://bossa.pl/index.jsp?layout=intraday&page=1&news_cat_id=875&dirpath=/mstock/daily/";
+            _intradayDataDownloader.Parser = _intradayDataParser;
+            _intradayDataDownloader.DownloadToFile(dialog.SelectedPath);
+        }
+    }
+}
