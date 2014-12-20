6e5e55a053bb2c565188147bedc1aba567dab567
landeeyo <landeeyo@10g.pl>
2014-12-20 13:57:52 +0100

    Added OxyPlot sample to WPF UI.

---
+++ StockTools/StockTools/StockTools.Domain/Concrete/Strategies/StubbornKGHMStrategy.cs
@@ -0,0 +1,24 @@
+﻿using StockTools.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTools.BusinessLogic.Concrete.Strategies
+{
+    public class StubbornKGHMStrategy : IStrategy
+    {
+        public List<Entities.Order> GenerateOrders(IArchivePriceProvider priceProvider, IPortfolio portfolio, DateTime dateTime)
+        {
+            ////If price is lower over 2% than buy price then sell and wait 3 days, if price is higher over 5% than buy price then sell and wait 3 days.
+
+            //var kghm = portfolio.Items.Where(x=>x.CompanyName == "KGHM").SingleOrDefault();
+            //var isBought = kghm != null && kghm.NumberOfShares > 0;
+
+            //var lastKghmTransaction = portfolio.Transactions.Where(x => x.CompanyName == "KGHM").OrderBy(x => x.Time).FirstOrDefault();
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StockTools/StockTools/StockTools.WPFUI/Models/PlotViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace StockTools.WPFUI.Models
+{
+    public class PlotViewModel
+    {
+        public PlotViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public PlotModel MyModel { get; private set; }
+    }
+}
